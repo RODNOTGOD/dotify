0954f61c4f20a9b676334d68419aaf4e5e42267f
Stefen Ramirez <stfnrmz0@gmail.com>
2018-11-11 21:15:27 -0600

    Start basic main layout with Music Player intent
    Create the basic layout for the application where all the Fragments
    will be shown. The Fragments in the system will propagate in the
    MainLayout file. Each button pressed will create a fragment to replace
    the previous fragment.
    
    Signed-off-by: Stefen Ramirez <stfnrmz0@gmail.com>

--- app/src/main/java/com/dotify/music/dotify/MusicPlayer.java
+++ app/src/main/java/com/dotify/music/dotify/MusicPlayer.java
@@ -1,57 +1,47 @@
 package com.dotify.music.dotify;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MusicPlayer extends Fragment {
+public class MusicPlayer extends AppCompatActivity {
 
-    int counter;
-    static MUSIC_STATUS status;
-    enum MUSIC_STATUS { PLAYING, PAUSE }
+    ImageView albumartView;
+    ImageView playView;
 
-    public MusicPlayer() {
-        counter = 0;
-        status = MUSIC_STATUS.PAUSE;
-    }
+    Button prevBtn;
+    Button playBtn;
+    Button nextBtn;
+
+    enum MusicListenStatus {PLAYING, PAUSE}
+
+    MusicListenStatus musicStatus;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
+        setContentView(R.layout.music_player);
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.music_player_fragment, container, false);
+        albumartView = findViewById(R.id.albumart_view);
+        playView = findViewById(R.id.player_view);
 
-        ImageButton playBtn = rootView.findViewById(R.id.music_play_button);
-        ImageButton prevBtn = rootView.findViewById(R.id.music_prev_button);
-        ImageButton nextBtn = rootView.findViewById(R.id.music_next_button);
+        prevBtn = findViewById(R.id.musicplayer_prev);
+        playBtn = findViewById(R.id.musicplayer_play);
+        nextBtn = findViewById(R.id.musicplayer_next);
 
-        playBtn.setOnClickListener(v -> {
-            if (status == MUSIC_STATUS.PAUSE) {
-                playBtn.setImageResource(R.drawable.music_play_background);
-                status = MUSIC_STATUS.PLAYING;
+        musicStatus = MusicListenStatus.PAUSE;
+
+        playBtn.setOnClickListener((v) -> {
+            if (musicStatus == MusicListenStatus.PAUSE) {
+                playView.setImageResource(R.drawable.music_pause_light);
+                musicStatus = MusicListenStatus.PLAYING;
             } else {
-                playBtn.setImageResource(R.drawable.music_pause_background);
-                status = MUSIC_STATUS.PAUSE;
+                playView.setImageResource(R.drawable.music_play_light);
+                musicStatus = MusicListenStatus.PAUSE;
             }
         });
 
-        prevBtn.setOnClickListener(v -> {
-            // go to previous song
-        });
-
-        prevBtn.setOnClickListener(v -> {
-            // go to previous song
-        });
-
-        return rootView;
     }
 }
