d65d3a3f325507c54a3a84950f396d042ea65664
Anastasia Stephenson <stephenson.ana@gmail.com>
2018-12-10 11:32:23 -0600

    Added a barebones playlist and user class

--- app/src/main/java/com/dotify/music/dotify/MainActivity.java
+++ app/src/main/java/com/dotify/music/dotify/MainActivity.java
@@ -14,6 +14,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static User currentUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
